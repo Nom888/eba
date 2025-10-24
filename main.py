b166c2e2ee93787458b6f65fba0f0ba4673e1482
Nom888 <nomo666006@gmail.com>
2025-10-24 11:42:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,8 @@ async def vless(session):
         if not server:
             return
         server, port = server.rsplit(":", 1)
+        print(server)
+        print(port)
         await asyncio.sleep(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
         await icmplib.async_ping(server, count=1, timeout=5)
         async with lock: VLESS_PING.append(server)
