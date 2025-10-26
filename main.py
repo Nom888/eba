7888001e6b7c0d92eba944edfc38362c46e87c67
Nom888 <nomo666006@gmail.com>
2025-10-26 23:03:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ async def cr(session, lock):
                         xtime = str(int(time.time()))
                         a = "{"
                         b = "}"
-                        body_string = f'{{"decorationPicUrl":"http://static.sandboxol.com/sandbox/avatar/male.png","inviteCode":"","details":"httрs://t.mе/kn_ew (in telegram @kn_ew)\\nBruteforce account","decorationPicUrl":"http://staticgs.sandboxol.com/avatar/1761081787482114.jpg","nickName":"{nickname}","picType":1,"sex":1}}'
+                        body_string = f'{{"decorationPicUrl":"http://static.sandboxol.com/sandbox/avatar/male.png","inviteCode":"","details":"httрs://t.mе/kn_ew (in telegram @kn_ew)\\nBruteforce account","decorationPicUrl":"http://staticgs.sandboxol.com/avatar/1761508908718930.jpg","nickName":"{nickname}","picType":1,"sex":1}}'
                         xsign = get_xsign(f"/user/api/v1/user/register", nonce, xtime, body_string, android_id)
                         async with session.post(
                             f"{random.choice(WORKERS)}https/gw.sandboxol.com/user/api/v1/user/register",
