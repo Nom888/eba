e6da813d5973875dba282426d42bfb5b746c3d6e
Nom888 <nomo666006@gmail.com>
2025-11-18 00:21:01 +0300

    Update and rename main.cpp to main.py

---
+++ main.py
@@ -0,0 +1,225 @@
+ENDPOINTS = "/user/api/v1/account/email/tp-auth/login/code/send,/user/api/v1/account/tp/list-public,/user/api/v1/account/email/reset-pwd/code/send,/user/api/v1/account/email/reset-pwd/code/verify,/user/api/v1/account/email/reset-password,/user/api/v1/account/email/register/code/send,/user/api/v1/account/email/register/code/verify,/user/api/v1/account/email/register,/user/api/v1/account/email/login/code/send,/user/api/v1/account/email/login/code/verify,/user/api/v1/account/email/login,/user/api/v1/account/invalid/check,/game/api/v1/appeal/game/list/get,/user/api/v1/appeal/info/handle,/user/api/v1/appeal/other/info/save,/user/api/v1/appeal/password/reset,/user/api/v1/user/random/nickname,/user/api/v3/users/verify/user/security/settings,/user/api/v1/vc/cimage,/user/api/v1/user/details/info/get,/pay/api/v1/coda/shop/top/up/verify,/pay/api/v1/xiao/mi/pay/notify,/editorproxy/community-api/v1/pub/community_news/detail,/editorproxy/meditor-api/v1/pub/lang_config_info,/activity/api/v1/garena/user/wealth,/user/api/v1/temporary/login,/editorproxy/community-api/v1/pub/community_news/list,/clan/api/v3/clan/tribe/recommendation,/user/api/v1/app/renew,/user/api/v1/user/data/delete,/editorproxy/meditor-api/v1/editor/version,/user/api/v1/temporary/uniworlds/login,/user/api/v1/login/encrypt,/pay/api/v1/xsolla/pay/billing/update,/user/api/v1/user/nickname/exist,/user/api/v1/users/third-part/info,/editorproxy/gamedata-api/v2/pub/game_manage/new_game_types,/user/api/v3/register,/user/api/v4/account/login,/editorproxy/meditor-api/v1/pub/meditor_templates,/user/api/v1/new/appeal/status,/user/api/v1/bmx/users/security/bind/email,/activity/api/v1/garena/user/game/data,/editorproxy/community-api/v1/pub/community_course/details,/editorproxy/gamedata-api/v1/pub/game_manage/game_types,/pay/api/v1/xsolla/enable,/editorproxy/gamedata-api/v1/game_show/find_game,/editorproxy/meditor-api/v1/pub/temporary_service/info,/user/api/v1/vc/validate,/user/api/v1/ip/region/get,/editorproxy/meditor-api/v1/pub/game/app_engine/check_update,/pay/api/v1/subscribe/notify/ios,/decoration/api/v1/decoration/versions,/user/api/v1/emails/reset-pwd,/user/api/v1/check/region/status,/user/api/v1/users/question/reset/password,/user/api/v1/xiao/mi/login,/pay/api/v1/subscribe/notify/google,/api/v1/users/third-part/info,/editorproxy/community-api/v1/pub/community_news/cumulative_view,/activity/api/v1/garena/user/info,/user/api/v1/pre-registration,/user/api/v1/users/security/verify/email/reset,/editorproxy/meditor-api/v1/pub/config/account_retrieve,/editorproxy/community-api/v1/pub/community_course/cumulative_view,/pay/api/v1/xsolla/billing/update,/user/api/v1/users/third-part/info/,/clan/api/v1/clan/tribe/recommendation,/activity/api/v1/recharge/count/info,/user/api/v5/account/auth-token,/user/api/v1/temporary/uniworlds/register,/user/api/v1/new/appeal/info/handle,/editorproxy/community-api/v1/pub/material/query_recommended_materials,/user/api/v1/token/info/get,/user/api/v1/emails/reset-pwd/auth,/user/api/v1/emails/password/reset,/datareport/api/v1/inner/statistics/userGameActionData,/datareport/api/v1/app/google/pay/process/get,/user/api/v2/app/renew,/decoration/api/v1/decorations/user-decoration,/user/api/v2/temporary/login,/user/api/v1/visitor,/user/api/v1/users/secret/question,/mailbox/api/v1/garena/mail/send,/notice/api/v1/server-notice/config,/auth/api/v1/users/third-part/info/,/pay/public/pay/api/v1/public/pay/testCountry,/editorproxy/community-api/v1/pub/community_news_sort/list,/user/api/v1/hua/wei/login,/editorproxy/community-api/v1/pub/community_course/list,/editorproxy/meditor-api/v1/pub/meditor_template,/user/api/v1/temporary/lock/region,/user/api/v2/login,/user/api/v1/user/login/change/record,/pay/api/v1/xsolla/validateuser,/user/api/v1/user/mac/id,/msg/api/v1/msg/receive_message,/pay/api/v1/xsolla/verifyuser,/user/api/v3/login,/user/api/v1/temporary/check/nickname/exist,/pay/api/v1/hua/wei/pay/notify,/pay/api/v1/public/pay/notify,/pay/api/v1/subscribe/google/msg/receive,/editorproxy/community-api/v1/pub/material/list,/editorproxy/community-api/v1/pub/material/production_guide_url,/pay/api/v1/codapay/notify,/pay/api/v1/razer/notify,/user/api/v1/lock/region,/pay/api/v1/public/pay/centili/notify,/pay/api/v1/coda/shop/validate/verify,/user/api/v1/check/account/source,/user/api/v3/account/tourist/login,/editorproxy/community-api/v1/pub/community_course_sort/list,/user/api/v1/temporary/register,/editorproxy/version-api/v1/check_launcher_update,/pay/api/v1/xsolla/billing/get,/datareport/api/v1/inner/statistics/userActionData,/msg/api/v1/msg/app/status,/statistic/api/v1/statistic/user/new/add/country,/pay/api/v1/xsolla/verifyUser,/user/api/v2/users/verify/user/security/settings,/pay/api/v1/xsolla/pay,/pay/api/v1/subscribe/ios/msg/receive,/editorproxy/meditor-api/v1/pub/lang_map/list,/user/api/v4/account/register,/editorproxy/community-api/v1/pub/community_course/list/with_page,/game/api/v1/games/so-resource/upgrade,/user/api/v1/users/security/bind/email,/activity/api/v1/garena/web/config,/user/api/v1/alerts/phones,/user/api/v1/region/recommend/list,/activity/api/v1/garena/user/item,/user/api/v1/sms/send/refound,/activity/api/v1/tradplus/reward-video/verify,/user/api/v2/app/login,/api/v1/users/third-part/info,/user/api/v1/user/password,/pay/api/v1/subscribe/garena/notify,/user/api/v1/device/bind/info/get,/user/api/v1/check/region/open/status,/config/files/game/pkg/check-version,/config/files/game/pkg/ad,/user/api/v1/account/tp/login,/user/api/v1/account/pkg/grant-login,/config/files/,/config/ml/files/".split(",")
+
+import asyncio
+import base64
+import random
+import uuid
+import time
+import hashlib
+import re
+
+import aiohttp
+from Crypto.Cipher import AES, PKCS1_v1_5
+from Crypto.Util.Padding import pad
+from Crypto.PublicKey import RSA
+from PyRoxy import ProxyChecker, ProxyUtiles
+
+PROXY_WORK = []
+
+def get_enc_query(bmg_id, x_nonce):
+    key = RSA.import_key(base64.b64decode("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCLzlsA+3wXCAph80r/xs1bWhVrsJSOQmSBTA0GaBpVIzXqFBaibDmYA3WJDM9rcQ7KpYSyrJ02iFlsN43RnizrHfS+xPtdwuxBQ2Clow5cYPZucqQYL9HIlbBLoighH2eGQqGlVadL7r384iKTz9mmckSUa8hhJzS+WwUAqVO3DwIDAQAB"))
+    cipher = PKCS1_v1_5.new(key)
+    query = f"0\n{bmg_id}\n{x_nonce}".encode()
+    encrypted = b""
+    for i in range(0, len(query), 117):
+        chunk = query[i:i+117]
+        encrypted += cipher.encrypt(chunk)
+    return base64.b64encode(encrypted).decode()
+
+def get_x_sign(x_urlpath, x_nonce, x_time, params, bmg_id):
+    md5 = hashlib.md5(f"6aDtpIdzQdgGwrpP6HzuPA{x_urlpath}{x_nonce}{x_time}{params}9EuDKGtoWAOWoQH1cRng-d5ihNN60hkGLaRiaZTk-6s".encode()).hexdigest()
+
+    if x_urlpath in ENDPOINTS or (x_urlpath.startswith("/config/files/") or x_urlpath.startswith("/config/ml/files/")):
+        return md5
+    return hashlib.md5(f"{md5}{device_id}".encode()).hexdigest()
+
+def get_bmg_sign(bmg_id):
+    xored_data = bytes(b ^ 0x73 for b in bmg_id.encode())
+    padded_data = pad(xored_data, AES.block_size)
+
+    cipher = AES.new(b"MFwwDQYJKoZIhvcN", AES.MODE_ECB)
+    encrypted_bytes = cipher.encrypt(padded_data)
+
+    return base64.b64encode(encrypted_bytes).decode()
+
+async def create_account(session):
+    while True:
+        bmg_id = "".join(random.choice("0123456789abcdef") for _ in range(16))
+        bmg_sign = get_bmg_sign(bmg_id)
+        x_nonce = str(uuid.uuid4())
+        x_time = str(int(time.time()))
+        x_urlpath = "/user/api/v5/account/auth-token"
+        q = get_enc_query(bmg_id, x_nonce)
+        x_sign = get_x_sign(x_urlpath, x_nonce, x_time, f"q={q}", bmg_id)
+        async with session.get(
+            f"http://{random.choice(["gw.sandboxol.com", 'gwbyte.sandboxol.com'])}/user/api/v5/account/auth-token",
+            params={"q": q},
+            headers={
+                "bmg-user-id": "0",
+                "bmg-device-id": bmg_id,
+                "bmg-sign": bmg_sign,
+                "bmg-adid-sign": "b915e18f539d8830fb0bebba670ed63b9cbd1053",
+                "package-name": "com.sandboxol.blockymods.official",
+                "userId": "0",
+                "packageName": "official",
+                "packageNameFull": "com.sandboxol.blockymods.official",
+                "androidVersion": "30",
+                "OS": "android",
+                "appType": "android",
+                "appLanguage": "en",
+                "appVersion": "5421",
+                "appVersionName": "2.125.1",
+                "channel": "sandbox",
+                "uid_register_ts": "0",
+                "device_register_ts": "0",
+                "eventType": "app",
+                "userDeviceId": bmg_id,
+                "userLanguage": "en_US",
+                "region": "",
+                "clientType": "client",
+                "env": "prd",
+                "package_name_en": "com.sandboxol.blockymods.official",
+                "md5": "c0c2f5baf2e9b4a063fc0cdf099960de",
+                "adid": "aae9dcee-9754-4e82-9378-e36618159408",
+                "telecomOper": "unknown",
+                "manufacturer": "Redmi_Redmi Note 8 Pro",
+                "network": "wifi",
+                "brand": "Redmi",
+                "model": "Redmi Note 8 Pro",
+                "device": "begonia",
+                "deviceModel": "Redmi Note 8 Pro",
+                "board": "begonia",
+                "cpu": "CPU architecture: 8",
+                "cpuFrequency": "2012500",
+                "dpi": "2.75",
+                "screenHeight": "2220",
+                "screenWidth": "1080",
+                "ram_memory": "5635",
+                "rom_memory": "52438",
+                "open_id": "",
+                "open_id_type": "0",
+                "client_ip": "",
+                "apps_flyer_gaid": "aae9dcee-9754-4e82-9378-e36618159408",
+                "X-ApiKey": "6aDtpIdzQdgGwrpP6HzuPA",
+                "X-Nonce": x_nonce,
+                "X-Time": x_time,
+                "X-Sign": x_sign,
+                "X-UrlPath": x_urlpath,
+                "Access-Token": "",
+                "Connection": "Keep-Alive",
+                "Accept-Encoding": "gzip",
+                "User-Agent": "okhttp/4.11.0"
+            }
+        ) as response:
+            print(await response.text())
+            await asyncio.sleep(90)
+
+async def get_proxy(session):
+    if "_" == "_":
+        global PROXY_WORK
+
+        prx = [
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/socks5.txt",
+            "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/socks5/data.txt",
+            "https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/refs/heads/main/proxy_files/socks5_proxies.txt",
+            "https://raw.githubusercontent.com/elliottophellia/proxylist/refs/heads/master/results/socks5/global/socks5_checked.txt",
+            "https://raw.githubusercontent.com/ProxyScraper/ProxyScraper/refs/heads/main/socks5.txt",
+            "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks5_proxies.txt",
+           # "https://raw.githubusercontent.com/zenjahid/FreeProxy4u/refs/heads/main/socks5.txt",
+            "https://raw.githubusercontent.com/databay-labs/free-proxy-list/refs/heads/master/socks5.txt",
+            "https://raw.githubusercontent.com/Skillter/ProxyGather/refs/heads/master/proxies/working-proxies-socks5.txt",
+            #"https://raw.githubusercontent.com/fyvri/fresh-proxy-list/archive/storage/classic/socks5.txt",
+            "https://raw.githubusercontent.com/zebbern/Proxy-Scraper/refs/heads/main/socks5.txt"
+        ]
+
+        send = 0
+        total = 0
+        naxui = random.randint(1000, 9999)
+        for url in prx:
+            send += 1
+            try:
+                async with session.get(url, timeout=5) as response:
+                    req = await response.text()
+                req = re.sub(r"^\s+|\s+$", "", re.sub(r"^\s*$\n?", "", req, flags=re.MULTILINE), flags=re.MULTILINE).splitlines()
+
+                if "socks5" in url:
+                    req = ["socks5://" + prox.lstrip("socks5://") for prox in req]
+
+                elif "socks4" in url:
+                    req = ["socks4://" + prox.lstrip("socks4://") for prox in req]
+
+                elif "https" in url:
+                    req = ["https://" + prox.lstrip("https://") for prox in req]
+
+                elif "http" in url:
+                    req = ["http://" + prox.lstrip("http://") for prox in req]
+
+                total += len(req)
+                print(f"[{send}]", url, f"| {len(req)}")
+
+                with open(f"bazadian{naxui}.txt", "a", encoding="utf-8") as f:
+                    f.write("\n".join([re.sub(r"^(([^:]+:){2}[^:]+):.*$", r"\1", prox) for prox in req]) + "\n")
+            except Exception as e:
+                print(e)
+
+        print(f"total {total}\n")
+
+        proxies_list = ProxyUtiles.readFromFile(f"bazadian{naxui}.txt")
+
+        loop = asyncio.get_running_loop()
+        result = await loop.run_in_executor(
+            None, ProxyChecker.checkAll, proxies_list
+        )
+        print("all proxy checked")
+
+        SOCKS = [str(proxy) for proxy in result]
+
+        print(f"Found {len(SOCKS)} working proxies.")
+        print(PROXY_WORK)
+
+"""
+async def get_proxy(session):
+    global PROXY
+
+    async with session.get("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/http/data.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/https/data.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://github.com/zloi-user/hideip.me/raw/refs/heads/master/http.txt") as response:
+        PROXY += [":".join(line.split(":")[:2]) for line in (await response.read()).decode(errors="ignore").splitlines()]
+    async with session.get("https://github.com/zloi-user/hideip.me/raw/refs/heads/master/https.txt") as response:
+        PROXY += [":".join(line.split(":")[:2]) for line in (await response.read()).decode(errors="ignore").splitlines()]
+    async with session.get("https://raw.githubusercontent.com/officialputuid/KangProxy/refs/heads/KangProxy/http/http.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/refs/heads/main/proxy_files/http_proxies.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/refs/heads/main/proxy_files/https_proxies.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/elliottophellia/proxylist/refs/heads/master/results/http/global/phttp_checked.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/ProxyScraper/ProxyScraper/refs/heads/main/http.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/http_proxies.txt") as response:
+        PROXY += (await response.text()).splitlines()
+    async with session.get("https://raw.githubusercontent.com/zenjahid/FreeProxy4u/refs/heads/main/http.txt") as response:
+        PROXY += (await response.text()).splitlines()
+
+    PROXY = list(set(PROXY))
+    print(PROXY)
+"""
+
+async def main():
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(ssl=False, limit=0),
+        skip_auto_headers={"Accept"}
+    ) as session:
+        await get_proxy(session)
+        await asyncio.gather(*[create_account(session) for _ in range(1)])
+
+try:
+    import uvloop
+    uvloop.run(main())
+except ModuleNotFoundError:
+    asyncio.run(main())
